0a58fcc2f0d156b5e07dd0ec55e51db809787a2a
Daniel Gavidia <dgavidia1@gmail.com>
2024-09-25 17:47:29 -0400

    Moved Prisma modules to node_modules

--- prisma/migrations/20240925214625_/migration.sql
+++ prisma/migrations/20240925214625_/migration.sql
@@ -1,26 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Client` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Item` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Order` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "Item" DROP CONSTRAINT "Item_orderId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Order" DROP CONSTRAINT "Order_clientId_fkey";
-
--- DropTable
-DROP TABLE "Client";
-
--- DropTable
-DROP TABLE "Item";
-
--- DropTable
-DROP TABLE "Order";
-
 -- CreateTable
 CREATE TABLE "Book" (
     "id" SERIAL NOT NULL,
